f3d4c884facab9ab1176ebcba7a97eca9c6c3694
naivoder <naivoder@gmail.com>
2024-06-20 16:06:02 -0400

    modified train loop to flexibly handle network architectures

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-from dataset import ImageDataset
-from networks import LaplacianNet
+from dataset import ImageDataset, ImagePyramidDataset
+from networks import LaplacianNet, CNNNet
 from utils import plot_confusion_matrix, plot_curves, plot_roc_auc
 
 import numpy as np
@@ -7,23 +7,26 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 
-def train_and_evaluate(dataset_name, train_data, test_data, device, n_epochs=100):
+def train_and_evaluate(dataset_name, train_data, test_data, device, n_epochs=100, cnn_only=False):
     train_indices, val_indices = train_test_split(range(len(train_data)), test_size=0.2, stratify=train_data.targets)
     train_subset = Subset(train_data, train_indices)
     val_subset = Subset(train_data, val_indices)
     
-    train_loader = DataLoader(ImageDataset(train_subset), batch_size=128, shuffle=True)
-    val_loader = DataLoader(ImageDataset(val_subset), batch_size=128, shuffle=False)
-    test_loader = DataLoader(ImageDataset(test_data), batch_size=128, shuffle=False)
+    DatasetClass, NetworkClass = (ImageDataset, CNNNet) if cnn_only else (ImagePyramidDataset, LaplacianNet)
+    
+    train_loader = DataLoader(DatasetClass(train_subset), batch_size=128, shuffle=True)
+    val_loader = DataLoader(DatasetClass(val_subset), batch_size=128, shuffle=False)
+    test_loader = DataLoader(DatasetClass(test_data), batch_size=128, shuffle=False)
 
     num_classes = len(train_data.classes)
     input_shape = train_data[0][0].shape
     
-    model = LaplacianNet(input_shape, num_classes).to(device)
+    model = NetworkClass(input_shape, num_classes).to(device)
     optimizer = optim.Adam(model.parameters(), lr=1e-4, amsgrad=True)
     criterion = nn.CrossEntropyLoss()
 
@@ -36,12 +39,15 @@ def train_and_evaluate(dataset_name, train_data, test_data, device, n_epochs=100
         model.train()
         running_loss = 0.0
         for inputs, labels in tqdm(train_loader):
-            inputs = [inp.to(device) for inp in inputs]
+            if cnn_only:
+                inputs = inputs.to(device)
+            else:
+                inputs = [inp.to(device) for inp in inputs]
             labels = labels.to(device)
             labels = nn.functional.one_hot(labels, num_classes=num_classes).float()
             
             optimizer.zero_grad()
-            outputs = model(inputs, labels)
+            outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -58,7 +64,10 @@ def train_and_evaluate(dataset_name, train_data, test_data, device, n_epochs=100
         correct, total = 0, 0
         with torch.no_grad():
             for inputs, labels in val_loader:
-                inputs = [inp.to(device) for inp in inputs]
+                if cnn_only:
+                    inputs = inputs.to(device)
+                else:
+                    inputs = [inp.to(device) for inp in inputs]
                 labels = labels.to(device)
                 labels = nn.functional.one_hot(labels, num_classes=num_classes).float()
                 
@@ -104,7 +113,10 @@ def train_and_evaluate(dataset_name, train_data, test_data, device, n_epochs=100
     correct, total = 0, 0
     with torch.no_grad():
         for inputs, labels in test_loader:
-            inputs = [inp.to(device) for inp in inputs]
+            if cnn_only:
+                inputs = inputs.to(device)
+            else:
+                inputs = [inp.to(device) for inp in inputs]
             labels = labels.to(device)
             labels = nn.functional.one_hot(labels, num_classes=num_classes).float()
             
@@ -112,6 +124,7 @@ def train_and_evaluate(dataset_name, train_data, test_data, device, n_epochs=100
             
             pred = outputs.argmax(dim=1)
             labels = labels.argmax(dim=1)
+            
             correct += (pred == labels).sum().item()
             total += labels.size(0)
             
