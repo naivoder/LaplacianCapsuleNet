e94314160d2c599d545e2788deacd51014f2864f
naivoder <naivoder@gmail.com>
2024-06-20 16:05:40 -0400

    added equivalent CNNNet for comparison

--- networks.py
+++ networks.py
@@ -1,16 +1,42 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.init as initializers
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import numpy as np
-import torchvision
-import torchvision.transforms as transforms
 import unittest
-from dataset import ImageDataset
-import os
 
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, input_dim):
+        super(CNNBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.ln1 = nn.LayerNorm([out_channels, input_dim, input_dim])
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.ln2 = nn.LayerNorm([out_channels, input_dim // 2, input_dim // 2])
+        self.dropout = nn.Dropout(0.1)
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = torch.relu(self.ln1(self.conv1(x)))
+        x = self.pool(x)
+        x = torch.relu(self.ln2(self.conv2(x)))
+        x = self.dropout(x)
+        return x
+
+class CNNNet(nn.Module):
+    def __init__(self, input_shape, num_classes):
+        super(CNNNet, self).__init__()
+        self.fc_input = 256 * 16 * 16
+        self.block1 = CNNBlock(input_shape[0], 64, 128)
+        self.block2 = CNNBlock(64, 128, 64)
+        self.block3 = CNNBlock(128, 256, 32)
+        self.fc = nn.Linear(self.fc_input, 128)
+        self.output = nn.Linear(128, num_classes)
+        print('Using CNNNet')
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = x.view(x.size(0), -1)
+        x = nn.functional.relu(self.fc(x))
+        return self.output(x)
 
 class PyramidBlock(nn.Module):
     def __init__(self, in_channels, out_channels, input_dim):
@@ -38,17 +64,16 @@ class LaplacianNet(nn.Module):
         self.pyramid_block3 = PyramidBlock(128 + input_shape[0], 256, 32)
         self.fc = nn.Linear(self.fc_input, 128)
         self.output = nn.Linear(128, num_classes)
+        print('Using LaplacianNet')
 
-    def forward(self, inputs, y=None):
+    def forward(self, inputs):
         x_l1, x_l2, x_l3, x_l4 = inputs
 
         x1 = self.pyramid_block1(x_l1)
         x2 = self.pyramid_block2(torch.cat((x1, x_l2), dim=1))
         x3 = self.pyramid_block3(torch.cat((x2, x_l3), dim=1))
         x = torch.cat([x3, x_l4], dim=1)
-        # print(x.size())
         x = x.view(x.size(0), -1)
-        # print(x.size())
         x = nn.functional.relu(self.fc(x))
         return self.output(x)
 
